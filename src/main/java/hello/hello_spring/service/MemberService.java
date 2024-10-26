1e12e78916cbc083e4c7a2ed67130fce931f6ebc
Gibeom Choi <kcdevdes@gmail.com>
2024-10-25 23:52:07 -0400

    Add MemberService

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,36 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository repository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        // 중복 방지
+        validateDuplicateMember(member);
+
+        repository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        repository
+                .findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("Duplicate User");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return repository.findAll();
+    }
+
+    public Optional<Member> findMember(Long id) {
+        return repository.findById(id);
+    }
+}
