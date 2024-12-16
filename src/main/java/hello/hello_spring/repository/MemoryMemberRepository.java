5409db12ed8be8c0f6de2de8612486893c8154ff
Gibeom Choi <kcdevdes@gmail.com>
2024-12-16 15:25:40 -0500

    feat: add Member Service

--- src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -4,9 +4,9 @@ import hello.hello_spring.domain.Member;
 
 import java.util.*;
 
-public class MemoryMemberRepository implements MemberRepository{
+public class MemoryMemberRepository implements MemberRepository {
 
-    private static Map<Long, Member> store = new HashMap<>();
+    private static final Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
     @Override
--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -2,15 +2,18 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service // Spring이 관리하는 서비스로 등록
 public class MemberService {
 
     private final MemberRepository repository;
 
+    @Autowired // 스프링이 스프링 컨테이너에 있는 MemberRepository 빈을 가져와 연결시켜줌
     public MemberService(MemberRepository repository) {
         this.repository = repository;
     }
